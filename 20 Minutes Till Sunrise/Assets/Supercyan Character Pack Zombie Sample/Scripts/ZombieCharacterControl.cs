f88de447f6c4d9daa60af27ec44de41825bc4054
zacharytgray <96278173+zacharytgray@users.noreply.github.com>
2023-04-19 18:35:05 -0500

    Plant,Zombie, and map implemented.
    Bounds are working so the player can't walk off. zombies and player die after 3 hits

--- 20 Minutes Till Sunrise/Assets/Supercyan Character Pack Zombie Sample/Scripts/ZombieCharacterControl.cs
+++ 20 Minutes Till Sunrise/Assets/Supercyan Character Pack Zombie Sample/Scripts/ZombieCharacterControl.cs
@@ -8,7 +8,9 @@ public class ZombieCharacterControl : MonoBehaviour
 
     [SerializeField] private Animator m_animator = null;
     [SerializeField] private Rigidbody m_rigidBody = null;
-
+    public int health = 3;
+    public GameObject zombie;
+    public GameObject bulletPrefab;
 
 
     public Transform player; // Reference to the player's transform
@@ -24,15 +26,39 @@ public class ZombieCharacterControl : MonoBehaviour
             
     
         // Calculate the direction from the zombie to the player
-        Vector3 direction = player.position - transform.position;
-        direction.y = 0; // Ignore the height difference
+        Vector3 direction = transform.position;
+        if (player != null) {
+            direction = player.position - transform.position;
+            direction.y = 0; // Ignore the height difference
 
-        // Rotate the zombie to face the player
-        transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(direction), m_turnSpeed);
+            // Rotate the zombie to face the player
+            transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(direction), m_turnSpeed);
 
-        // Move the zombie towards the player
-        transform.position += transform.forward * m_moveSpeed * Time.deltaTime;
+            // Move the zombie towards the player
+            transform.position += transform.forward * m_moveSpeed * Time.deltaTime;
+        }
+
+        if (health <= 0) {
+            Destroy(zombie.gameObject);
+        }
+        
     }
+
+    void OnTriggerEnter(Collider other) {
+        Transform rootT = other.gameObject.transform.root;
+        GameObject go = rootT.gameObject;
+
+
+        Bullet enemy = go.GetComponent<Bullet>();
+        if (enemy != null) {
+            Destroy(go);
+            health--;
+        }
+
+    }
+
+
+
 }
  
     
--- 20 Minutes Till Sunrise/Assets/_Scripts/PlayerController.cs
+++ 20 Minutes Till Sunrise/Assets/_Scripts/PlayerController.cs
@@ -12,41 +12,54 @@ public class PlayerController : MonoBehaviour
     public GameObject playerObject;
 
     public GameObject bulletSpawnPoint;
-    public float coolDown;
+
+    public int playerHealth = 3;
+    public bool isAlive = true;
 
     void Update() {
-        Plane playerPlane = new Plane(Vector3.up, transform.position);
-        Ray ray = UnityEngine.Camera.main.ScreenPointToRay(Input.mousePosition);
-        float hitDist = 0.0f;
+        if (isAlive && this.gameObject != null) {
+            Plane playerPlane = new Plane(Vector3.up, transform.position);
+            Ray ray = UnityEngine.Camera.main.ScreenPointToRay(Input.mousePosition);
+            float hitDist = 0.0f;
 
-        if (playerPlane.Raycast(ray, out hitDist)) {
-            Vector3 targetPoint = ray.GetPoint(hitDist);
-            Quaternion targetRotation = Quaternion.LookRotation(targetPoint - transform.position);
-            targetRotation.x = 0;
-            targetRotation.z = 0;
-            playerObject.transform.rotation = Quaternion.Slerp(playerObject.transform.rotation, targetRotation, 7f * Time.deltaTime);
+            if (playerPlane.Raycast(ray, out hitDist)) {
+                Vector3 targetPoint = ray.GetPoint(hitDist);
+                Quaternion targetRotation = Quaternion.LookRotation(targetPoint - transform.position);
+                targetRotation.x = 0;
+                targetRotation.z = 0;
+                playerObject.transform.rotation = Quaternion.Slerp(playerObject.transform.rotation, targetRotation, 7f * Time.deltaTime);
+            }
+
+            // Movement
+            if (Input.GetKey(KeyCode.W)) {
+                transform.Translate(Vector3.forward * movementSpeed * Time.deltaTime);
+            }
+
+            if (Input.GetKey(KeyCode.A)) {
+                transform.Translate(Vector3.left * movementSpeed * Time.deltaTime);
+            }
+
+            if (Input.GetKey(KeyCode.S)) {
+                transform.Translate(Vector3.back * movementSpeed * Time.deltaTime);
+            }
+
+            if (Input.GetKey(KeyCode.D)) {
+                transform.Translate(Vector3.right * movementSpeed * Time.deltaTime);
+            }
+            // Shooting
+            if (Input.GetMouseButtonDown(0)) {
+                Shoot();
+            }
         }
 
-        // Movement
-        if (Input.GetKey(KeyCode.W)) {
-            transform.Translate(Vector3.forward * movementSpeed * Time.deltaTime);
+        bool getIsAlive() {
+            return isAlive;
         }
 
-        if (Input.GetKey(KeyCode.A)) {
-            transform.Translate(Vector3.left * movementSpeed * Time.deltaTime);
-        }
 
-        if (Input.GetKey(KeyCode.S)) {
-            transform.Translate(Vector3.back * movementSpeed * Time.deltaTime);
-        }
-
-        if (Input.GetKey(KeyCode.D)) {
-            transform.Translate(Vector3.right * movementSpeed * Time.deltaTime);
-        }
-
-        // Shooting
-        if (Input.GetMouseButtonDown(0)) {
-            Shoot();
+        if (playerHealth <= 0) {
+            Destroy(this.gameObject);
+            isAlive = false;
         }
 
     }
@@ -54,6 +67,12 @@ public class PlayerController : MonoBehaviour
     void Shoot() {
         // Instantiate(bullet.transform, bulletSpawnPoint.transform.position, Quaternion.identity);
         Instantiate(bullet.transform, bulletSpawnPoint.transform.position, bulletSpawnPoint.transform.rotation);
+    }
+
+    void OnCollisionEnter(Collision col) {
+        if (col.gameObject.name == "Zombie") {
+            playerHealth--;
+        }
     }
     
 }
